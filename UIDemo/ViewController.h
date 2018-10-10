8a4e8b7fada68104b6896a1d7f621be0ff5473c6
tielan.fu <>
2018-10-10 09:21:39 +0800

    初始化

---
+++ UIDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIDemo
+//
+//  Created by iosteams on 2018/10/10.
+//  Copyright © 2018年 tielan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UIDemo/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  UIDemo
+//
+//  Created by iosteams on 2018/10/10.
+//  Copyright © 2018年 tielan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.title = @"UIDemo";
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
